a1186fda3c5f72f6f7ec0f138ab386fe6c25b35f
Nazar <kubik.nazar.work@gmail.com>
2023-08-30 16:41:56 +0300

    feat: setup validation pipe & add global prefix

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+(async () => {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+  const logger = new Logger('API');
+
+  const configService = app.get(ConfigService);
+
+  const port = configService.get('PORT', 3000);
+  const prefix = configService.get('API_PREFIX', 'api');
+
+  app.setGlobalPrefix(prefix);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+      forbidUnknownValues: true,
+    }),
+  );
+
+  await app.listen(port, () => {
+    logger.log(`Listening on port: ${port}`);
+  });
+})();

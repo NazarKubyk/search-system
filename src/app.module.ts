dc58da4dd6fbc2352af674ebb1eccd8f73846cf2
Nazar <kubik.nazar.work@gmail.com>
2023-08-30 16:38:46 +0300

    feat: define config and cache modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
+import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { CqrsModule } from '@nestjs/cqrs';
 import { AppController } from './app.controller';
+import { CacheConfigService } from './configs/cache.config';
 
 @Module({
-  imports: [],
+  imports: [
+    CqrsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      useClass: CacheConfigService,
+    }),
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
